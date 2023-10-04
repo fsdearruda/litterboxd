b0eca8790e090cb88e836c73036e9bb661381aba
rapoxo <fernandojs006@gmail.com>
2023-10-03 23:42:20 -0300

    lista de gêneros dos filmes

--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -1,23 +1,85 @@
-const genres: { [key: number]: string } = {
-  28: "Ação",
-  12: "Aventura",
-  16: "Animação",
-  35: "Comédia",
-  80: "Crime",
-  99: "Documentário",
-  18: "Drama",
-  10751: "Família",
-  14: "Fantasia",
-  36: "História",
-  27: "Terror",
-  10402: "Música",
-  9648: "Mistério",
-  10749: "Romance",
-  878: "Ficção científica",
-  10770: "Cinema TV",
-  53: "Thriller",
-  10752: "Guerra",
-  37: "Faroeste",
+const genreList: {
+  [key: number]: {
+    name: string;
+    emoji: string;
+  };
+} = {
+  28: {
+    name: "Ação",
+    emoji: "💥",
+  },
+  12: {
+    name: "Aventura",
+    emoji: "🌍",
+  },
+  16: {
+    name: "Animação",
+    emoji: "🐰",
+  },
+  35: {
+    name: "Comédia",
+    emoji: "😄",
+  },
+  80: {
+    name: "Crime",
+    emoji: "🔎",
+  },
+  99: {
+    name: "Documentário",
+    emoji: "📖",
+  },
+  18: {
+    name: "Drama",
+    emoji: "🎭",
+  },
+  10751: {
+    name: "Família",
+    emoji: "👨‍👩‍👧‍👦",
+  },
+  14: {
+    name: "Fantasia",
+    emoji: "🧙‍♂️",
+  },
+  36: {
+    name: "História",
+    emoji: "🕰️",
+  },
+  27: {
+    name: "Terror",
+    emoji: "🧟‍♂️",
+  },
+  10402: {
+    name: "Música",
+    emoji: "🎶",
+  },
+  9648: {
+    name: "Mistério",
+    emoji: "🔍",
+  },
+  10749: {
+    name: "Romance",
+    emoji: "❤️",
+  },
+  878: {
+    name: "Ficção científica",
+    emoji: "🚀",
+  },
+  10770: {
+    name: "Cinema TV",
+    emoji: "📺",
+  },
+  53: {
+    name: "Thriller",
+    emoji: "😱",
+  },
+  10752: {
+    name: "Guerra",
+    emoji: "🔫",
+  },
+  37: {
+    name: "Faroeste",
+    emoji: "🤠",
+  },
 };
 
-export default genres;
+export { genreList };
