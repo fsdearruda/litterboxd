8c116410a401c4b19a458e96f09fa875d1e3f796
rapoxo <fernandojs006@gmail.com>
2023-10-03 23:41:47 -0300

    mudanças nos tipos

--- src/@types/movie.ts
+++ src/@types/movie.ts
@@ -1,14 +1,32 @@
+type Provider = {
+  logo_path: string;
+  provider_id: number;
+  provider_name: string;
+  display_priority: number;
+};
+type ProviderResult = {
+  link: string;
+  rent: Provider[];
+  buy: Provider[];
+  flatrate: Provider[];
+};
+
 type Movie = {
   id: number;
   title: string;
+  rating: number;
   original_title: string;
   overview: string;
-  genres: string[];
   release_date: string;
+  genres: {
+    name: string;
+    emoji: string;
+  }[];
   images: {
     backdrop: string;
     poster: string;
   };
+  providers?: ProviderResult;
 };
 
-export default Movie;
+export { Movie, Provider, ProviderResult };
