2f8a6d0c8769793cf819ef3b89705e0c016b0aee
rapoxo <fernandojs006@gmail.com>
2023-10-22 00:47:37 -0300

    fix: changing file extension when code is compiled

--- src/utils/loadCommands.ts
+++ src/utils/loadCommands.ts
@@ -1,27 +1,21 @@
 import { ClientWithCommands, FileExport } from "../@types";
-import {
-  Collection,
-  REST,
-  RESTPostAPIChatInputApplicationCommandsJSONBody,
-  Routes,
-} from "discord.js";
+import { Collection, REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from "discord.js";
 
 import { readdir } from "fs/promises";
 import path from "path";
 
 const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DISCORD_GUILD_ID, DISCORD_DEV_GUILD_ID } = process.env;
 
-const loadCommands = async (
-  client: ClientWithCommands,
-  root: string
-): Promise<void> => {
+const loadCommands = async (client: ClientWithCommands, root: string): Promise<void> => {
   const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
   client.commands = new Collection();
 
   const commandsPath = path.join(root, "commands");
-  const commandFiles = (await readdir(commandsPath)).filter((file) =>
-    file.endsWith(".ts")
-  );
+
+  // Addressing the issue of the file extension changing when compiled to JS
+  const currentFileExtension = path.extname(__filename);
+
+  const commandFiles = (await readdir(commandsPath)).filter(file => file.endsWith(currentFileExtension));
 
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
@@ -31,27 +25,20 @@ const loadCommands = async (
       client.commands.set(command.data.name, command);
       commands.push(command.data.toJSON());
     } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
   }
 
   const rest = new REST().setToken(DISCORD_TOKEN as string);
 
   try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    const data = (await rest.put(
-      Routes.applicationGuildCommands(DISCORD_CLIENT_ID as string, DISCORD_DEV_GUILD_ID ?? DISCORD_GUILD_ID as string),
-      { body: commands }
-    )) as Array<any>;
+    const data = (await rest.put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID as string, DISCORD_DEV_GUILD_ID ?? (DISCORD_GUILD_ID as string)), {
+      body: commands,
+    })) as Array<any>;
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (err) {
     console.error(err);
   }
